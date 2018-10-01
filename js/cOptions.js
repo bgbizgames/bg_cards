2bb28efcb4147f3a18fc3725f062846d81bd9871
gernot <gernot.faschinger@bizgames.at>
2018-10-01 10:24:32 +0200

    paths to bgcards corrected

--- js/cOptions.js
+++ js/cOptions.js
@@ -1,5 +1,5 @@
 var OPTIONS = {
-  imgbase: '/cardlearning/assets/',     //root folder for images
+  imgbase: '/bgcards/assets/',     //root folder for images
   btnfont: "15px Arial",           //font for buttons
   gamewidth: 1024,                      //width of game canvas
   gameheight: 768,                      //height of game canvas
